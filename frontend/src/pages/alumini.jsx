fcc2694f976e3852e3a9f8b2d374c870a0c4aec3
Hruthik <mhrithik450@gmail.com>
2024-10-06 15:26:54 +0530

    eighteencommit

--- frontend/src/pages/alumini.jsx
+++ frontend/src/pages/alumini.jsx
@@ -1,4 +1,6 @@
 import Hero from "../components/hero";
+import Slider from "../components/slider";
+import Slider3 from "../components/slider3";
 import "./alumini.css";
 
 import React from "react";
@@ -13,6 +15,96 @@ const Alumini = () => {
           subtitle: "",
         }}
       />
+
+      <section>
+        <div className="home_main">
+          <div className="left_part">
+            <Slider
+              dataURLS={[
+                "https://res.cloudinary.com/duozomapm/image/upload/v1728141010/vangoghmuseum-s0144V1962-800_bcicci.jpg",
+                "https://res.cloudinary.com/duozomapm/image/upload/v1728133868/vangoghmuseum-s0050V1962-800_1_sfbgt0.jpg",
+                "https://res.cloudinary.com/duozomapm/image/upload/v1728141036/vangoghmuseum-s0031V1962-800_oladul.jpg",
+                "https://res.cloudinary.com/duozomapm/image/upload/v1728125218/vangoghmuseum-s0202V1962-800_gccwd1.jpg",
+                "https://res.cloudinary.com/duozomapm/image/upload/v1728140989/vangoghmuseum-s0109V1962-800_yhdl4y.jpg",
+                "https://res.cloudinary.com/duozomapm/image/upload/v1728141070/vangoghmuseum-s0102V1962-800_ofsdqr.jpg",
+              ]}
+            />
+          </div>
+          <div className="right_part">
+            <p style={{ color: "gray" }}>
+              Vincent Willem van Gogh was a Dutch Post-Impressionist painter who
+              is among the most famous and influential figures in the history of
+              Western art. In just over a decade, he created approximately 2100
+              artworks, including around 860 oil paintings, most of them in the
+              last two years of his life.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="library-section-2">
+        <h3>Activities</h3>
+        <div className="library-main">
+          <Slider3
+            slides={[
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728133868/vangoghmuseum-s0050V1962-800_1_sfbgt0.jpg",
+                title: "Alumni Records",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728125218/vangoghmuseum-s0202V1962-800_gccwd1.jpg",
+                title: "Membership",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728140989/vangoghmuseum-s0109V1962-800_yhdl4y.jpg",
+                title: "Reunions",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728141036/vangoghmuseum-s0031V1962-800_oladul.jpg",
+                title: "Scholarships",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728141010/vangoghmuseum-s0144V1962-800_bcicci.jpg",
+                title: "Give to UVCE",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728139435/Screenshot_2024-09-23_234428_cl3fxl.jpg",
+                title: "Programmes",
+              },
+            ]}
+          />
+        </div>
+      </section>
+
+      <section className="library-section-2 alumini-section-3">
+        <h3>Alumini Blogs</h3>
+        <div className="alumini-sec-3">
+          <div className="alumini-image first-img">
+            <h3>Future of Energy - Dr Prahlada Rama Rao</h3>
+
+            <img
+              src="https://res.cloudinary.com/duozomapm/image/upload/v1728188596/vangoghmuseum-s0028V1962-800_vpp4u0.jpg"
+              alt="image2"
+            />
+          </div>
+
+          <div className="alumini-image second-img">
+            <h3>Blockchain and its applications</h3>
+            <img
+              src="https://res.cloudinary.com/duozomapm/image/upload/v1728188614/vangoghmuseum-s0051V1962-800_gskvns.jpg"
+              alt="image2"
+            />
+          </div>
+
+          <div className="alumini-image third-img">
+            <h3>Advances in Aerospace Engineering - Gopalkrishna</h3>
+            <img
+              src="https://res.cloudinary.com/duozomapm/image/upload/v1728123644/vangoghmuseum-s0105V1962-800_wwerrm.jpg"
+              alt="image1"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
--- frontend/src/pages/library.jsx
+++ frontend/src/pages/library.jsx
@@ -1,5 +1,6 @@
 import Hero from "../components/hero";
 import Slider from "../components/slider";
+import Slider3 from "../components/slider3";
 import "./library.css";
 
 import React from "react";
@@ -39,12 +40,116 @@ const Library = () => {
             <span>Search Book/Author/Keywords</span>
           </div>
 
-          <div>Hello</div>
+          <div>Google form</div>
         </div>
       </section>
 
       <section className="library-section-2">
         <h3>Library Resources</h3>
+        <div className="library-main">
+          <Slider3
+            slides={[
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728133868/vangoghmuseum-s0050V1962-800_1_sfbgt0.jpg",
+                title: "Inflibnet",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728125218/vangoghmuseum-s0202V1962-800_gccwd1.jpg",
+                title: "Old Question Papers",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728140989/vangoghmuseum-s0109V1962-800_yhdl4y.jpg",
+                title: "E-Newspapers",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728141036/vangoghmuseum-s0031V1962-800_oladul.jpg",
+                title: "E-Journals",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728141010/vangoghmuseum-s0144V1962-800_bcicci.jpg",
+                title: "Publications",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728139435/Screenshot_2024-09-23_234428_cl3fxl.jpg",
+                title: "E-Books",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728139460/Roses_-_The_Metropolitan_Museum_of_Art_xltmy4.jpg",
+                title: "Library Subscriptions",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728185750/vangoghmuseum-s0025V1962-800_yptwqa.jpg",
+                title: "Plagiarism Tool",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728125218/vangoghmuseum-s0202V1962-800_gccwd1.jpg",
+                title: "Memberships",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728123585/vangoghmuseum-s0032V1962-800_sffk94.jpg",
+                title: "Reprography",
+              },
+            ]}
+          />
+        </div>
+      </section>
+
+      <section className="library-section-3">
+        <h3>Library Team</h3>
+        <div className="library-main-2">
+          <img
+            src="https://res.cloudinary.com/duozomapm/image/upload/v1728099382/vangoghmuseum-s0093V1962-800_rkhwip.jpg"
+            alt=""
+          />
+
+          <h3>Sandhyarani K</h3>
+        </div>
+      </section>
+
+      <section className="library-section-3">
+        <h3>Librarian</h3>
+        <div className="library-main">
+          <Slider3
+            slides={[
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728188136/vangoghmuseum-s0022V1962-800_tvrlfm.jpg",
+                title: "W",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728104256/vangoghmuseum-s0016V1962-800_bhvmdm.jpg",
+                title: "X",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728099339/vangoghmuseum-s0097V1962v-800_qlcqlc.jpg",
+                title: "Y",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728099387/vangoghmuseum-s0065V1962-800_xjut4d.jpg",
+                title: "Z",
+              },
+              {
+                url: "https://res.cloudinary.com/duozomapm/image/upload/v1728099404/vangoghmuseum-s0060V1962v-800_slam8p.jpg",
+                title: "A",
+              },
+            ]}
+          />
+        </div>
+      </section>
+
+      <section>
+        <div className="library-main-3">
+          <Slider
+            dataURLS={[
+              "https://res.cloudinary.com/duozomapm/image/upload/v1728188542/vangoghmuseum-s0104V1962-800_klzfxe.jpg",
+              "https://res.cloudinary.com/duozomapm/image/upload/v1728188614/vangoghmuseum-s0051V1962-800_gskvns.jpg",
+              "https://res.cloudinary.com/duozomapm/image/upload/v1728188556/vangoghmuseum-s0038V1962-800_bu4vsc.jpg",
+              "https://res.cloudinary.com/duozomapm/image/upload/v1728123578/vangoghmuseum-s0047V1962-800_gnefn4.jpg",
+              "https://res.cloudinary.com/duozomapm/image/upload/v1728123585/vangoghmuseum-s0032V1962-800_sffk94.jpg",
+              "https://res.cloudinary.com/duozomapm/image/upload/v1728188596/vangoghmuseum-s0028V1962-800_vpp4u0.jpg",
+              "https://res.cloudinary.com/duozomapm/image/upload/v1728123631/vangoghmuseum-s0055V1962-800_oqy5hq.jpg",
+            ]}
+          />
+        </div>
       </section>
     </>
   );
